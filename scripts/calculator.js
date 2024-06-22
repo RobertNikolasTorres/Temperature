982da37daa85de477784615eb3a6ad35948190e3
RobertNikolasTorres <robert.nikolas.torres@gmail.com>
2024-06-22 12:12:44 -0700

    calculator

---
+++ scripts/calculator.js
@@ -0,0 +1,33 @@
+console.log("Calculator script");
+let num1 = prompt("Enter the first number");
+let num2 = prompt("Enter the second number");
+let operation = prompt("Select the operation: 1.Addition, 2.Subtraction, 3.Multiplication, 4.Division.");
+
+console.log(num1);
+console.log(num2);
+console.log(operation);
+
+if(operation==1){
+    //do the operation
+    let result = Number(num1) + Number(num2);
+    //display result
+    console.log(result);
+}
+if(operation==2){
+    //do the operation
+    let result = Number(num1) - Number(num2);
+    //display result
+    console.log(result);
+}
+if(operation==3){
+    //do the operation
+    let result = Number(num1) * Number(num2);
+    //display result
+    console.log(result);
+}
+if(operation==4){
+    //do the operation
+    let result = Number(num1) / Number(num2);
+    //display result
+    console.log(result);
+}
---
+++ scripts/scripts1.js
@@ -0,0 +1,26 @@
+console.log("Script1");
+
+let example = "Example";
+var example2 = "Example 2";
+
+let example3;
+example3 = 10;
+console.log(example);
+console.log(example2);
+console.log(example3);
+
+//String type variable
+let strVar = "Example";
+//Number type variable
+let nmbVar = 5;
+//Boolean type variable
+let boo1Var = true;
+console.log(nmbVar+example3);
+console.log(example+example2);
+
+
+//How to get a value from a prompt
+let myAge =Number(prompt("Enter your age"))
+console.log(myAge);
+console.log(typeof(myAge));
+console.log(10 + myAge);
